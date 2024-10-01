34777c4770394791fea2b0175715c918c0d8c3e0
Dylan Carter <carterdw@hendrix.edu>
2024-10-01 01:01:54 -0500

    added RSVP status button

--- firebase_demo/lib/app_state.dart
+++ firebase_demo/lib/app_state.dart
@@ -8,9 +8,27 @@ import 'package:flutter/material.dart';
 import 'guest_book_message.dart'; 
 import 'firebase_options.dart';
 
+enum Attending { yes, no, unknown }
 class ApplicationState extends ChangeNotifier {
   ApplicationState() {
     init();
+  }
+  int _attendees = 0;
+int get attendees => _attendees;
+  Attending _attending = Attending.unknown;
+  StreamSubscription<DocumentSnapshot>? _attendingSubscription;
+
+  Attending get attending => _attending;
+
+  set attending(Attending attending) {
+    final userDoc = FirebaseFirestore.instance
+        .collection('attendees')
+        .doc(FirebaseAuth.instance.currentUser!.uid);
+    if (attending == Attending.yes) {
+      userDoc.set(<String, dynamic>{'attending': true});
+    } else {
+      userDoc.set(<String, dynamic>{'attending': false});
+    }
   }
 Future<DocumentReference> addMessageToGuestBook(String message) {
     if (!_loggedIn) {
@@ -39,8 +57,16 @@ Future<DocumentReference> addMessageToGuestBook(String message) {
 
     FirebaseUIAuth.configureProviders([
       EmailAuthProvider(),
+      
     ]);
-    
+    FirebaseFirestore.instance
+        .collection('attendees')
+        .where('attending', isEqualTo: true)
+        .snapshots()
+        .listen((snapshot) {
+      _attendees = snapshot.docs.length;
+      notifyListeners();
+    });
     FirebaseAuth.instance.userChanges().listen((user) {
       if (user != null) {
         _loggedIn = true;
@@ -60,10 +86,27 @@ Future<DocumentReference> addMessageToGuestBook(String message) {
           }
           notifyListeners();
         });
+        _attendingSubscription = FirebaseFirestore.instance
+            .collection('attendees')
+            .doc(user.uid)
+            .snapshots()
+            .listen((snapshot) {
+          if (snapshot.data() != null) {
+            if (snapshot.data()!['attending'] as bool) {
+              _attending = Attending.yes;
+            } else {
+              _attending = Attending.no;
+            }
+          } else {
+            _attending = Attending.unknown;
+          }
+          notifyListeners();
+        });
       } else {
         _loggedIn = false;
         _guestBookMessages = [];
         _guestBookSubscription?.cancel();
+        _attendingSubscription?.cancel();
       }
       notifyListeners();
     });
