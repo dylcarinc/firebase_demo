2c3692f301e3e68549a66fa7789700fd9a7a3134
Dylan Carter <carterdw@hendrix.edu>
2024-10-01 00:27:58 -0500

    Allowed the app to write messages to the firestore

--- firebase_demo/lib/home_page.dart
+++ firebase_demo/lib/home_page.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart' // new
     hide EmailAuthProvider, PhoneAuthProvider;    // new
 import 'package:flutter/material.dart';           // new
+import 'package:gtk_flutter/guest_book.dart';
 import 'package:provider/provider.dart';          // new
 
 import 'app_state.dart';                          // new
 import 'src/authentication.dart';                 // new
 import 'src/widgets.dart';
+import 'guest_book.dart';  
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -41,6 +43,20 @@ class HomePage extends StatelessWidget {
           const Header("What we'll be doing"),
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
+          ),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loggedIn) ...[
+                  const Header('Discussion'),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                  ),
+                ],
+              ],
+            ),
           ),
         ],
       ),
